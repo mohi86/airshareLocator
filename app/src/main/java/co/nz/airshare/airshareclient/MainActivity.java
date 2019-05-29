b9826a5271e47f83344a8cf7b9149fa9f4afee9c
Mohi Khalili <mohi86@live.com>
2019-05-30 08:03:20 +1200

    Kiosk mode added

--- app/src/main/java/co/nz/airshare/airshareclient/MainActivity.java
+++ app/src/main/java/co/nz/airshare/airshareclient/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -18,14 +19,28 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     LocationManager locationManager;
     LocationListener locationListener;
+    Boolean isTracking = false;
+    Button trackingButton;
+    TextView latTextView;
+    TextView longTextView;
+    TextView accTextView;
+    TextView altTextView;
+
+    @Override
+    public void onBackPressed() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +48,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         startService(new Intent(this, AirshareLocationService.class));
+
+        trackingButton = findViewById(R.id.trackingButton);
+        latTextView = findViewById(R.id.latTextView);
+        longTextView = findViewById(R.id.longTextView);
+        accTextView = findViewById(R.id.accTextView);
+        altTextView = findViewById(R.id.altTextView2);
 
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 
@@ -60,11 +81,11 @@ public class MainActivity extends AppCompatActivity {
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-        } else {
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,1000, 0,locationListener );
-            Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            if (lastKnownLocation != null) {
-                updateLocationInfo(lastKnownLocation);
+        } else{
+            if (isTracking) {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,2000, 0,locationListener );
+                Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                if (lastKnownLocation != null) updateLocationInfo(lastKnownLocation);
             }
         }
     }
@@ -73,8 +94,22 @@ public class MainActivity extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED && isTracking) {
             startListening();
+        }
+    }
+
+    public void startButtonClicked(View view) {
+        if (isTracking != true) {
+            isTracking = true;
+            trackingButton.setText("Stop");
+            trackingButton.setBackgroundColor(Color.parseColor("#FFB935"));
+            startListening();
+        } else {
+            isTracking = false;
+            trackingButton.setText("Start");
+            trackingButton.setBackgroundColor(Color.parseColor("#29A3D0"));
+            stopListening();
         }
     }
 
@@ -84,19 +119,18 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public void stopListening() {
+        locationManager.removeUpdates(locationListener);
+    }
+
     public void updateLocationInfo(Location location) {
-        TextView latTextView = findViewById(R.id.latTextView);
-        TextView longTextView = findViewById(R.id.longTextView);
-        TextView accTextView = findViewById(R.id.accTextView);
-        TextView altTextView = findViewById(R.id.altTextView2);
 
-        Log.i("lat: ", Double.toString(location.getAccuracy()));
+        Log.i("lat: ", Double.toString(location.getLatitude()));
+        Log.i("long: ", Double.toString(location.getLongitude()));
 
-
-        latTextView.setText(String.format("Latitude: %s", location.getLatitude()));
-        longTextView.setText(String.format("Longitude: %s", location.getLongitude()));
-        accTextView.setText(String.format("Accuracy: %s", location.getAccuracy()));
-        altTextView.setText(String.format("Altitude: %s", location.getAltitude()));
-
+        latTextView.setText(String.format(Locale.getDefault(),"Latitude: %.5f", location.getLatitude()));
+        longTextView.setText(String.format(Locale.getDefault(),"Longitude: %.5f", location.getLongitude()));
+        accTextView.setText(String.format(Locale.getDefault(),"Accuracy: %s", location.getAccuracy()));
+        altTextView.setText(String.format(Locale.getDefault(),"Altitude: %s", location.getAltitude()));
     }
 }
