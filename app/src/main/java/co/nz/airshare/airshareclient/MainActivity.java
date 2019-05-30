0e4e9dda5ab918017e5029ccb6cf260bf28b3eb0
Mohi Khalili <mohi86@live.com>
2019-05-30 22:55:43 +1200

    Azure iot connection done

--- app/src/main/java/co/nz/airshare/airshareclient/MainActivity.java
+++ app/src/main/java/co/nz/airshare/airshareclient/MainActivity.java
@@ -2,8 +2,6 @@ package co.nz.airshare.airshareclient;
 
 import android.Manifest;
 import android.app.ActivityManager;
-import android.app.PendingIntent;
-import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -11,20 +9,36 @@ import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.os.IBinder;
+import android.os.Handler;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodSession;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import org.w3c.dom.Text;
+import com.microsoft.azure.sdk.iot.device.DeviceClient;
+import com.microsoft.azure.sdk.iot.device.DeviceTwin.DeviceMethodData;
+import com.microsoft.azure.sdk.iot.device.IotHubClientProtocol;
+import com.microsoft.azure.sdk.iot.device.IotHubConnectionStatusChangeCallback;
+import com.microsoft.azure.sdk.iot.device.IotHubConnectionStatusChangeReason;
+import com.microsoft.azure.sdk.iot.device.IotHubEventCallback;
+import com.microsoft.azure.sdk.iot.device.IotHubMessageResult;
+import com.microsoft.azure.sdk.iot.device.IotHubStatusCode;
+import com.microsoft.azure.sdk.iot.device.Message;
+import com.microsoft.azure.sdk.iot.device.transport.IotHubConnectionStatus;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URISyntaxException;
 
 import java.util.Locale;
 
@@ -32,6 +46,34 @@ public class MainActivity extends AppCompatActivity {
 
     LocationManager locationManager;
     LocationListener locationListener;
+    Double latitude;
+    Double longitude;
+    Double altitude;
+    float accuracy;
+
+    String connString = BuildConfig.ConnectionString;
+
+    private String msgStr;
+    private Message sendMessage;
+    private String lastException;
+
+    private DeviceClient client;
+
+    IotHubClientProtocol protocol = IotHubClientProtocol.MQTT;
+
+    private int msgSentCount = 0;
+    private int receiptsConfirmedCount = 0;
+    private int sendFailuresCount = 0;
+    private int msgReceivedCount = 0;
+    private int sendMessagesInterval = 1000;
+
+    private final Handler handler = new Handler();
+    private Thread sendThread;
+
+    private static final int METHOD_SUCCESS = 200;
+    public static final int METHOD_THROWS = 403;
+    private static final int METHOD_NOT_DEFINED = 404;
+
     Boolean isTracking = false;
     Button trackingButton;
     TextView latTextView;
@@ -40,20 +82,6 @@ public class MainActivity extends AppCompatActivity {
     TextView altTextView;
     TextView statusTextView;
 
-
-    @Override
-    public void onBackPressed() {
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        ActivityManager activityManager = (ActivityManager) getApplicationContext()
-                .getSystemService(Context.ACTIVITY_SERVICE);
-
-        activityManager.moveTaskToFront(getTaskId(), 0);
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -101,6 +129,248 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void stop()
+    {
+        new Thread(new Runnable() {
+            public void run()
+            {
+                try
+                {
+                    sendThread.interrupt();
+                    client.closeNow();
+                    System.out.println("Shutting down...");
+                }
+                catch (Exception e)
+                {
+                    lastException = "Exception while closing IoTHub connection: " + e;
+                    handler.post(exceptionRunnable);
+                }
+            }
+        }).start();
+    }
+
+    private void start()
+    {
+        sendThread = new Thread(new Runnable() {
+            public void run()
+            {
+                try
+                {
+                    initClient();
+                    for(;;)
+                    {
+                        sendMessages();
+                        Thread.sleep(sendMessagesInterval);
+                    }
+                }
+                catch (InterruptedException e)
+                {
+                    return;
+                }
+                catch (Exception e)
+                {
+                    lastException = "Exception while opening IoTHub connection: " + e;
+                    handler.post(exceptionRunnable);
+                }
+            }
+        });
+
+        sendThread.start();
+    }
+
+    Runnable exceptionRunnable = new Runnable() {
+        public void run() {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setMessage(lastException);
+            builder.show();
+            System.out.println(lastException);
+        }
+    };
+
+    Runnable methodNotificationRunnable = new Runnable() {
+        public void run() {
+            Context context = getApplicationContext();
+            CharSequence text = "Set Send Messages Interval to " + sendMessagesInterval + "ms";
+            int duration = Toast.LENGTH_LONG;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
+    };
+
+    private void sendMessages()
+    {
+        msgStr = "\"latitude\":" + String.format(Locale.getDefault(),"%s",latitude) + ", " +
+                "\"longitude\":" + String.format(Locale.getDefault(),"%s",longitude) +
+                ", \"altitude\":" + String.format(Locale.getDefault(),"%s",altitude);
+        try
+        {
+            sendMessage = new Message(msgStr);
+            sendMessage.setMessageId(java.util.UUID.randomUUID().toString());
+            System.out.println("Message Sent: " + msgStr);
+            EventCallback eventCallback = new EventCallback();
+            client.sendEventAsync(sendMessage, eventCallback, msgSentCount);
+            msgSentCount++;
+        }
+        catch (Exception e)
+        {
+            System.err.println("Exception while sending event: " + e);
+        }
+    }
+
+    private void initClient() throws URISyntaxException, IOException
+    {
+        client = new DeviceClient(connString, protocol);
+
+        try
+        {
+            client.registerConnectionStatusChangeCallback(new IotHubConnectionStatusChangeCallbackLogger(), new Object());
+            client.open();
+            MessageCallback callback = new MessageCallback();
+            client.setMessageCallback(callback, null);
+            client.subscribeToDeviceMethod(new SampleDeviceMethodCallback(), getApplicationContext(), new DeviceMethodStatusCallBack(), null);
+        }
+        catch (Exception e)
+        {
+            System.err.println("Exception while opening IoTHub connection: " + e);
+            client.closeNow();
+            System.out.println("Shutting down...");
+        }
+    }
+
+    class EventCallback implements IotHubEventCallback
+    {
+        public void execute(IotHubStatusCode status, Object context)
+        {
+            Integer i = context instanceof Integer ? (Integer) context : 0;
+            System.out.println("IoT Hub responded to message " + i.toString()
+                    + " with status " + status.name());
+
+            if((status == IotHubStatusCode.OK) || (status == IotHubStatusCode.OK_EMPTY))
+            {
+                receiptsConfirmedCount++;
+            }
+            else
+            {
+                sendFailuresCount++;
+            }
+        }
+    }
+
+    class MessageCallback implements com.microsoft.azure.sdk.iot.device.MessageCallback
+    {
+        public IotHubMessageResult execute(Message msg, Object context)
+        {
+            System.out.println(
+                    "Received message with content: " + new String(msg.getBytes(), Message.DEFAULT_IOTHUB_MESSAGE_CHARSET));
+            msgReceivedCount++;
+
+            return IotHubMessageResult.COMPLETE;
+        }
+    }
+
+    protected static class IotHubConnectionStatusChangeCallbackLogger implements IotHubConnectionStatusChangeCallback
+    {
+        @Override
+        public void execute(IotHubConnectionStatus status, IotHubConnectionStatusChangeReason statusChangeReason, Throwable throwable, Object callbackContext)
+        {
+            System.out.println();
+            System.out.println("CONNECTION STATUS UPDATE: " + status);
+            System.out.println("CONNECTION STATUS REASON: " + statusChangeReason);
+            System.out.println("CONNECTION STATUS THROWABLE: " + (throwable == null ? "null" : throwable.getMessage()));
+            System.out.println();
+
+            if (throwable != null)
+            {
+                throwable.printStackTrace();
+            }
+
+            if (status == IotHubConnectionStatus.DISCONNECTED)
+            {
+                //connection was lost, and is not being re-established. Look at provided exception for
+                // how to resolve this issue. Cannot send messages until this issue is resolved, and you manually
+                // re-open the device client
+            }
+            else if (status == IotHubConnectionStatus.DISCONNECTED_RETRYING)
+            {
+                //connection was lost, but is being re-established. Can still send messages, but they won't
+                // be sent until the connection is re-established
+            }
+            else if (status == IotHubConnectionStatus.CONNECTED)
+            {
+                //Connection was successfully re-established. Can send messages.
+            }
+        }
+    }
+
+    protected class SampleDeviceMethodCallback implements com.microsoft.azure.sdk.iot.device.DeviceTwin.DeviceMethodCallback
+    {
+        @Override
+        public DeviceMethodData call(String methodName, Object methodData, Object context)
+        {
+            DeviceMethodData deviceMethodData ;
+            try {
+                switch (methodName) {
+                    case "setSendMessagesInterval": {
+                        int status = method_setSendMessagesInterval(methodData);
+                        deviceMethodData = new DeviceMethodData(status, "executed " + methodName);
+                        break;
+                    }
+                    default: {
+                        int status = method_default(methodData);
+                        deviceMethodData = new DeviceMethodData(status, "executed " + methodName);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                int status = METHOD_THROWS;
+                deviceMethodData = new DeviceMethodData(status, "Method Throws " + methodName);
+            }
+            return deviceMethodData;
+        }
+    }
+
+    private int method_setSendMessagesInterval(Object methodData) throws UnsupportedEncodingException, JSONException
+    {
+        String payload = new String((byte[])methodData, "UTF-8").replace("\"", "");
+        JSONObject obj = new JSONObject(payload);
+        sendMessagesInterval = obj.getInt("sendInterval");
+        handler.post(methodNotificationRunnable);
+        return METHOD_SUCCESS;
+    }
+
+    private int method_default(Object data)
+    {
+        System.out.println("invoking default method for this device");
+        // Insert device specific code here
+        return METHOD_NOT_DEFINED;
+    }
+
+    protected class DeviceMethodStatusCallBack implements IotHubEventCallback
+    {
+        public void execute(IotHubStatusCode status, Object context)
+        {
+            System.out.println("IoT Hub responded to device method operation with status " + status.name());
+        }
+    }
+
+
+
+    @Override
+    public void onBackPressed() {
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        ActivityManager activityManager = (ActivityManager) getApplicationContext()
+                .getSystemService(Context.ACTIVITY_SERVICE);
+
+        activityManager.moveTaskToFront(getTaskId(), 0);
+    }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -116,11 +386,13 @@ public class MainActivity extends AppCompatActivity {
             trackingButton.setText("Stop");
             trackingButton.setBackgroundColor(Color.parseColor("#FFB935"));
             startListening();
+            start();
         } else {
             isTracking = false;
             trackingButton.setText("Start");
             trackingButton.setBackgroundColor(Color.parseColor("#29A3D0"));
             stopListening();
+            stop();
         }
     }
 
@@ -139,6 +411,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void updateLocationInfo(Location location) {
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+        altitude = location.getAltitude();
+        accuracy = location.getAccuracy();
+
         statusTextView.setVisibility(View.INVISIBLE);
         latTextView.setText(String.format(Locale.getDefault(),"Latitude: %.5f", location.getLatitude()));
         longTextView.setText(String.format(Locale.getDefault(),"Longitude: %.5f", location.getLongitude()));
